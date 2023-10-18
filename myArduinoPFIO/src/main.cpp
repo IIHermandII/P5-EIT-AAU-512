c73017b44164cc31d896299b6e16f760b9dfe8e8
HerpSlerp <asbjorn.herbsleb@gmail.com>
2023-10-18 11:13:42 +0200

    real update

--- myArduinoPFIO/src/main.cpp
+++ myArduinoPFIO/src/main.cpp
@@ -1,121 +1,176 @@
 #include <Arduino.h>
 
-const int analogPin = A3; // potentiometer wiper (middle terminal) connected to analog pin 3
+// Define PID constants
+float Kp = 1.0;
+float Ki = 0.1;
+float Kd = 0.1;
+
+// Define variables
+float setpoint = 90;  // Desired temperature
+float input, output, error;
+float last_error = 0;
+float integral = 0;
+int encoder_count = 0;
+int encoder_threshold = 1000;
+bool encoder_state_change = 0;
+
+
+// Define pins
+const int encoderPin = A3;
 const int Black = 11;
 const int Red = 9;
-int time_t = 0;
-bool state = false; //true starte = overshoot | false state = undershoot
-int lastSystemError = 0.3;
+
+// pwm global variables
+const int pwm_period = 50; // 20 kHz PWM period in microseconds
+unsigned long pre_micros = 0;
+int pwmValue = 0;
+int i, j = 0;
 
 
-int getInput() {
-  int input;
-  input = map(analogRead(analogPin), 0, 1023, 0, 255);
-  Serial.print("analog input value ----> ");
+// Interupt function
+void encoder_counter (){
+  encoder_count += 1;
+  Serial.print("count: ");
+  Serial.println(encoder_count);
+  // Serial.print("Read: ");
+  // Serial.println(analogRead(analogPin));
+}
+
+
+
+void pwm(int pin, int duty){
+  unsigned long current_micros = micros();
+  
+  if (current_micros - pre_micros >= duty){
+    digitalWrite(pin, LOW);
+    // Serial.print("0");
+    }
+
+  // Check if it's time to update the PWM signal
+  if (current_micros - pre_micros >= pwm_period) {
+    pre_micros = current_micros;
+    digitalWrite(pin, HIGH);
+    // Serial.print("1");
+
+  
+  }
+}
+
+float readSensor() {
+  float input;
+
+  input = analogRead(encoderPin); //
+  // Serial.print("Hall sensor input value ----> ");
   Serial.println(input);
+  if (input<encoder_threshold){
+    if (encoder_state_change){
+      encoder_counter ();
+      encoder_state_change = 0;
+    }
+  }
+  else{
+    encoder_state_change = 1;
+  }
+  // Serial.print("State: ");
+  // Serial.println(encoder_state_change);
+  // Serial.print("count: ");
+  // Serial.println(encoder_count);
+  // Serial.print("Read: ");
+  // Serial.println(analogRead(analogPin));
   return input;
 }
 
-
-float getRequestAngle(int angleRequest) {
-  float requestValue;
-  requestValue = (angleRequest * 360 / 255);
-  Serial.print("requested value ----> ");
-  Serial.println(requestValue);
-  return requestValue;
-}
-
-
-int getError(int inputValue, float requestValue) {
-  int error;
-  error = requestValue - inputValue;
-  Serial.print("error value ----> ");
-  Serial.println(error);
-  return error;
-}
-
-
-int getP(int K_p, int systemError) {
-  int P;
-  P = K_p * systemError;
-  Serial.print("P value ----> ");
-  Serial.println(P);
-  return P;
-}
-
-
-int getI(int K_i, int K_c, int systemError) {
-  int I;
-
-  I = K_c * (systemError + (K_i * lastSystemError));
-  Serial.print("I value ----> ");
-  Serial.println(I);
-  return I;
-}
-
-
-int getD(int K_d, int K_c, int systemError) {
-  int D;
-  D = K_c * (systemError - (K_d * lastSystemError));
-  Serial.print("D value ----> ");
-  Serial.println(D);
-  lastSystemError = systemError;
-  return D; // nice
-}
-
-
-void systemOutPID(int P, int I, int D){
-  int PID;
-  PID = P + I + D;
- 
-
-  if (PID >= 255) PID = 255;
-  if (PID <= -255) PID = -255; 
+void controlActuator(float output) {
+  int PID = output;
+  int max_pwm = 50;
+  // Serial.print("PWM: ");
+  // Serial.println(output);
+  if (PID >= max_pwm) PID = max_pwm;
+  if (PID <= -max_pwm) PID = -max_pwm; 
 
   if (PID >=0 ) {
-    analogWrite(Black, PID);
+    pwm(Black, PID);
     pinMode(Red, LOW);
     }
   else {
-    analogWrite(Red, -PID);
+    pwm(Red, -PID);
     pinMode(Black, LOW);
     } 
-  }
+}
 
-  
 void setup() {
+  // Initialize your hardware, e.g., temperature sensor and actuator
+  // Set up serial communication for debugging
   Serial.begin(9600);
-  pinMode(Black, OUTPUT); 
-  pinMode(Red, OUTPUT); 
+  pinMode(encoderPin, INPUT);
+
+  // Changing the PWM frequency on output pin:
+  //TIMER1 setup
+  TCCR1A=TCCR1B=0;
+  //set waveform generator mode WGM1 3..0: 1110 (mode 14; FAST PWM) clear OC1A on compare match
+  //set prescaler CS12..0: 100 (clkio/256)
+  TCCR1A = (1 << COM1A1) | (1 << WGM11); 
+  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS12);
+  //set top and compare register
+  ICR1 = 3124; //TOP for 20 Hz, 256 prescaler
+  OCR1A = 1000; //default PWM compare
+  
+  //on Mega2560, OC1A function is tied to pin 11 (9 on ATmega328)
+  pinMode( Red, OUTPUT );
+  pinMode( Black, OUTPUT );
+
 }
 
 
+
 void loop() {
-  int feedback;
-  int refference;
-  int systemError;
-  int angleRequest = 90;
-  int K_p = 0.4; // PID hvor P = K_p * e(t)
-  int K_i = 0.6; // PID hvor I = K_i * intg0->t{ e(t) dt }
-  int K_d = 0.2; // PID hvor D = K_d * diff{ e(t) }
-  int K_c = 0.3; // P D controler parameter 
-  int p_cap = 1;
-  int i_cap = 1;
-  int d_cap = 1;
-  int P;
-  int I;
-  int D;
+readSensor();  
+pwm(Red, 30);
 
+// if (i == 30000){
+//   i = 0;
+//   j ++ ;
+//   Serial.println(j);
+//   if (j == 50){
+//     j = 0;
+    
+//   }
+//   }
+// i++;
+  /*
+  // Read the current temperature from a sensor (replace with your actual sensor code)
+  input = readSensor();
 
-  feedback = getInput();  // gets the value from potensiometer and returns 0..255
-  refference = getRequestAngle(angleRequest); // gets the angle from request and returns 0..255
-  systemError = getError(feedback, refference); // gets the system error
-  P = getP(K_p, systemError); // gets P in PID
-  I = getI(K_i, K_c, systemError); // gets I in PID
-  D = getD(K_d, K_c, systemError); // gets D in PID
-  systemOutPID(P, I, D);
+  // Calculate the error
+  error = setpoint - input;
 
+  // Calculate the proportional term
+  float P = Kp * error;
 
+  // Calculate the integral term
+  integral += Ki * error;
 
-  delay(500);
+  // Calculate the derivative term
+  float derivative = Kd * (error - last_error);
+
+  // Calculate the control output
+  output = P + integral + derivative;
+  output = -125;
+  // Apply the control output to your actuator (e.g., a heater or cooler)
+  controlActuator(output);
+
+  // Store the current error for the next iteration
+  last_error = error;
+
+  // Print the values for debugging
+  // Serial.print("Input: ");
+  // Serial.print(input);
+  // Serial.print("  Output: ");
+  // Serial.print(output);
+  // Serial.print("  Error: ");
+  // Serial.println(error);
+
+  // Add a delay to control the loop update rate
+  // delay(1000);  // Adjust as needed
+  */
 }
