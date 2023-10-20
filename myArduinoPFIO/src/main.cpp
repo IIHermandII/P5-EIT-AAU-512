eab8ea57d9817db16430fc1c39a468c42efa1d9a
IIHermandII <hermand8320@gmail.com>
2023-10-20 11:35:38 +0200

    working PID moter driver, the vights need tuning

--- myArduinoPFIO/src/main.cpp
+++ myArduinoPFIO/src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 
 // Define PID constants
-float Kp = 1.0;
-float Ki = 0.1;
-float Kd = 0.1;
+float Kp = 1;
+float Ki = 0.05;
+float Kd = 0.05;
 
 // Define variables
 float setpoint = 90;  // Desired temperature
@@ -22,52 +22,36 @@ const int Black = 11;
 const int Red = 9;
 
 // pwm global variables
-const float pwm_period = 50; // (10kHz)20 kHz PWM period in microseconds
-unsigned long pre_micros = 0;
-int pwmValue = 0;
-int i, j = 0;
 bool clock_wise;
 
+struct PID_values {
+  float P;
+  float I;
+  float D;
+};
 
 // Interupt function
 void encoder_counter (){
+
   if (clock_wise){
+    if (encoder_count == 178) {
+      encoder_count = 0;
+    }
     encoder_count ++;
   }
   else{
-    encoder_count --;
-  }
-}
-
-
-
-void pwm(int pin, float duty){//duty [%] from 0-100 
-  unsigned long current_micros = micros();
-  
-  if (current_micros - pre_micros >= ((pwm_period/100)*duty)){
-    digitalWrite(pin, LOW);
+    if (encoder_count == 0) {
+      encoder_count = 178;
     }
-  if (current_micros - pre_micros >= pwm_period && duty != 0) {
-    pre_micros = current_micros;
-    digitalWrite(pin, HIGH);
-  }
-  else{
-    digitalWrite(pin, LOW);
+    encoder_count --;
   }
 }
 
 float readSensor() {
   float encoder_read;
   encoder_read = analogRead(encoderPin); //
-  if (encoder_read<encoder_threshold){
-    if (encoder_state_change){
-      encoder_state_change = 0;
-    }
-  }
-  else{
-    encoder_state_change = 1;
-  }
-  float angle = map((encoder_count%counts_per_rotation), 0, counts_per_rotation-1, 0, 359);
+  Serial.print("Encoder count : ");
+  float angle = map((encoder_count), 0, counts_per_rotation, 0, 359);
   return angle;
 }
 
@@ -87,6 +71,25 @@ void controlActuator(float output) {
     analogWrite(Red, -PID);
     pinMode(Black, LOW);
     } 
+  Serial.print("PID : ");
+  Serial.println(PID);
+}
+
+PID_values anti_overflow(float P, float I, float D){
+  PID_values holder;
+float arr[3] ={P,I,D};
+for (int i=0; i<3; i++){
+  if (arr[i] > 255){
+    arr[i] = 255;
+  }
+  if (arr[i] < -255){
+    arr[i] = -255;
+  }
+}
+  holder.P = arr[0];
+  holder.I = arr[1];
+  holder.D = arr[2];
+return holder;
 }
 
 void setup() {
@@ -99,13 +102,27 @@ void setup() {
 }
 
 void loop() {
-  input = readSensor();
+  input = readSensor();  //
+  //Serial.print("angle : ");
+  //Serial.println(input);
   error = setpoint - input;
   float P = Kp * error;
-  integral += Ki * error;
-  float derivative = Kd * (error - last_error);
+
+  integral += Ki * error;  // fix over flov !!!!!!
+
+
+  float derivative = Kd * (error - last_error);    
   output = P + integral + derivative;
-  output = -125;
+   PID_values result = anti_overflow(P, integral, derivative);
+   P = result.P;
+   integral = result.I;
+   derivative = result.D;
+  // Serial.print("P, I, D : ");
+  // Serial.print(P);
+  // Serial.print(" ");
+  // Serial.print(integral);
+  // Serial.print(" ");
+  // Serial.println(derivative);
   controlActuator(output);
   last_error = error;
 }
