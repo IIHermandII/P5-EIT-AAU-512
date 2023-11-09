8ad6f812fb9729384aabbaffbe1d08dee797a7d1
HerpSlerp <asbjorn.herbsleb@gmail.com>
2023-11-09 13:00:48 +0100

    python code for serial send test

--- serial_send_test.py
+++ serial_send_test.py
@@ -1,30 +1,12 @@
 import serial
 import time
 
-# arduino = serial.Serial(port='COM19',  baudrate=115200, timeout=.1)
+SerialObj = serial.Serial('COM20', baudrate=115200, bytesize=8, parity='N', stopbits=1)
 
+while True:
+    num = input("Enter a number: ")
+    SerialObj.write(bytes(num, 'utf-8'))  # Transmit input to Arduino
+    time.sleep(0.1)  # Optional delay to ensure data is sent before proceeding
 
-# def write_read(x):
-#     arduino.write(bytes(x,  'utf-8'))
-#     time.sleep(0.05)
-#     data = arduino.readline()
-#     return  data
-
-SerialObj = serial.Serial('COM20') # COMxx  format on Windows
-                  # ttyUSBx format on Linux
-SerialObj.baudrate = 115200  # set Baud rate to 9600
-SerialObj.bytesize = 8   # Number of data bits = 8
-SerialObj.parity  ='N'   # No parity
-SerialObj.stopbits = 1   # Number of Stop bits = 1
-
-while True
-    time.sleep(1000)
-    SerialObj.write(b'42')    #transmit 'A' (8bit) to micro/Arduino
-    SerialObj.close()      # Close the port
-# while True:
-#     num = input("Enter a number: ")
-#     value  = write_read(num)
-#     print(value)
-
-
-
+# Close the serial port when done (this line will not be reached in the current script)
+SerialObj.close()
