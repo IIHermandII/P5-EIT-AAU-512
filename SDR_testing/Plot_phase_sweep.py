52f7e4377e22504afe29fb12847875e29b2244b9
IIHermandII <hermand8320@gmail.com>
2023-11-10 10:20:36 +0100

    testing GNU GIT

---
+++ SDR_testing/Plot_phase_sweep.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Phase sweeper
+# GNU Radio version: 3.10.7.0
+
+from packaging.version import Version as StrictVersion
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio import blocks
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio.qtgui import Range, RangeWidget
+from PyQt5 import QtCore
+import Plot_phase_sweep_epy_block_1 as epy_block_1  # embedded python block
+import sip
+
+
+
+class Plot_phase_sweep(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Phase sweeper", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Phase sweeper")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "Plot_phase_sweep")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = int(5e6)
+        self.Gain = Gain = 0
+        self.Freq = Freq = int(2.4e9)
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self.Tabs = Qt.QTabWidget()
+        self.Tabs_widget_0 = Qt.QWidget()
+        self.Tabs_layout_0 = Qt.QBoxLayout(Qt.QBoxLayout.TopToBottom, self.Tabs_widget_0)
+        self.Tabs_grid_layout_0 = Qt.QGridLayout()
+        self.Tabs_layout_0.addLayout(self.Tabs_grid_layout_0)
+        self.Tabs.addTab(self.Tabs_widget_0, 'RF control')
+        self.top_grid_layout.addWidget(self.Tabs, 2, 0, 1, 1)
+        for r in range(2, 3):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self._Gain_range = Range(0, 80, 1, 0, 200)
+        self._Gain_win = RangeWidget(self._Gain_range, self.set_Gain, "'Gain'", "counter_slider", int, QtCore.Qt.Horizontal)
+        self.Tabs_grid_layout_0.addWidget(self._Gain_win, 1, 0, 1, 1)
+        for r in range(1, 2):
+            self.Tabs_grid_layout_0.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.Tabs_grid_layout_0.setColumnStretch(c, 1)
+        self._Freq_range = Range(int(2.4e9), int(2.48e9), int(1e6), int(2.4e9), 200)
+        self._Freq_win = RangeWidget(self._Freq_range, self.set_Freq, "'Freq'", "counter_slider", int, QtCore.Qt.Horizontal)
+        self.Tabs_grid_layout_0.addWidget(self._Freq_win, 0, 0, 1, 1)
+        for r in range(0, 1):
+            self.Tabs_grid_layout_0.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.Tabs_grid_layout_0.setColumnStretch(c, 1)
+        self.qtgui_number_sink_0 = qtgui.number_sink(
+            gr.sizeof_float,
+            0,
+            qtgui.NUM_GRAPH_HORIZ,
+            1,
+            None # parent
+        )
+        self.qtgui_number_sink_0.set_update_time(0.10)
+        self.qtgui_number_sink_0.set_title("Max angle")
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        units = ['', '', '', '', '',
+            '', '', '', '', '']
+        colors = [("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"),
+            ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black")]
+        factor = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+
+        for i in range(1):
+            self.qtgui_number_sink_0.set_min(i, -90)
+            self.qtgui_number_sink_0.set_max(i, 90)
+            self.qtgui_number_sink_0.set_color(i, colors[i][0], colors[i][1])
+            if len(labels[i]) == 0:
+                self.qtgui_number_sink_0.set_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_number_sink_0.set_label(i, labels[i])
+            self.qtgui_number_sink_0.set_unit(i, units[i])
+            self.qtgui_number_sink_0.set_factor(i, factor[i])
+
+        self.qtgui_number_sink_0.enable_autoscale(False)
+        self._qtgui_number_sink_0_win = sip.wrapinstance(self.qtgui_number_sink_0.qwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_number_sink_0_win, 1, 0, 1, 1)
+        for r in range(1, 2):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self.qtgui_const_sink_x_0 = qtgui.const_sink_c(
+            91, #size
+            "", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_const_sink_x_0.set_update_time(0.10)
+        self.qtgui_const_sink_x_0.set_y_axis((-50), 10)
+        self.qtgui_const_sink_x_0.set_x_axis((-90), 90)
+        self.qtgui_const_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
+        self.qtgui_const_sink_x_0.enable_autoscale(False)
+        self.qtgui_const_sink_x_0.enable_grid(False)
+        self.qtgui_const_sink_x_0.enable_axis_labels(True)
+
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ["blue", "red", "red", "red", "red",
+            "red", "red", "red", "red", "red"]
+        styles = [0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0]
+        markers = [0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_const_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_const_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_const_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_const_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_const_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_const_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_const_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.qwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_const_sink_x_0_win, 0, 0, 1, 1)
+        for r in range(0, 1):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self.epy_block_1 = epy_block_1.blk(SampleRate=samp_rate, SignalFreq=Freq, Gain=Gain, Rx1_Phase_Cal=0, Rx2_Phase_Cal=31, Rx3_Phase_Cal=355, Rx4_Phase_Cal=20)
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_float*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_moving_average_xx_0 = blocks.moving_average_ff(1000, (1/1000), samp_rate, 1)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_moving_average_xx_0, 0), (self.qtgui_number_sink_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.blocks_moving_average_xx_0, 0))
+        self.connect((self.epy_block_1, 1), (self.blocks_throttle2_0, 0))
+        self.connect((self.epy_block_1, 0), (self.qtgui_const_sink_x_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "Plot_phase_sweep")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
+        self.epy_block_1.SampleRate = self.samp_rate
+
+    def get_Gain(self):
+        return self.Gain
+
+    def set_Gain(self, Gain):
+        self.Gain = Gain
+        self.epy_block_1.Gain = self.Gain
+
+    def get_Freq(self):
+        return self.Freq
+
+    def set_Freq(self, Freq):
+        self.Freq = Freq
+        self.epy_block_1.SignalFreq = self.Freq
+
+
+
+
+def main(top_block_cls=Plot_phase_sweep, options=None):
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
