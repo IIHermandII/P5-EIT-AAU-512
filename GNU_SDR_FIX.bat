2de01b140dfe118686e574cc1326d1519d631cde
IIHermandII <hermand8320@gmail.com>
2023-11-10 12:51:58 +0100

    gnu auto pat bat

---
+++ GNU_SDR_FIX.bat
@@ -0,0 +1,25 @@
+@echo off
+
+echo Current directory: %cd%
+pushd  ".\SDR_testing\"
+set git_path_dll=%cd%\VNX_dps64.dll
+echo DLL path: %git_path_dll%
+
+cd /d C:\Users\%USERNAME%\OneDrive\Dokumenter
+dir
+@REM where we want the file to be 
+set doc_path=%cd%
+set doc_path_file=%cd%\VNX_dps64.dll
+
+echo le path : %doc_path%
+if exist "%doc_path_file%" (
+    echo VNX_dps64.dll alredy exist
+) else (
+    echo VNX_dps64.dll does not exist at %doc_path%
+    echo will copy the file from %git_path_dll%
+    copy "%git_path_dll%" .
+    echo file copyed you can now run Manual phase Control.grc
+)
+dir
+
+
