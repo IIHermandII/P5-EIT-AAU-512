1d510df8b426c4e1417393678257f758d9edc118
Hakapuff <caspernbak@gmail.com>
2023-11-30 09:41:46 +0100

    værsgo smukke

--- SDR_testing/data_manage.py
+++ SDR_testing/data_manage.py
@@ -2,30 +2,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 from datetime import datetime
 import os
+from pathlib import Path
 
-print(os.getcwd() + "###################################")
-# path = "C:\Users\caspe\OneDrive - Aalborg Universitet\GIT\P5-EIT-AAU-512\SDR_testing"
+# # Get the current script's path
+# current_script = os.path.realpath(__file__)
 
+# # Extract the folder path from the script's path
+# current_folder = os.path.dirname(current_script)
 
+# # Construct the folder path for the data files
+# text_file_position = os.path.join(current_folder, "data_folder")
 
+# # Print the folder path
+# print(text_file_position)
 
-date_time ="Steering_angles " + str(datetime.now())
-# # Load data from files
-# # f = open(r"C:\Users\caspe\OneDrive - Aalborg Universitet\GIT\P5-EIT-AAU-512\SDR_testing\Steering_angles.txt", "r")
-# # f = open(r"C:\Users\caspe\OneDrive - Aalborg Universitet\GIT\P5-EIT-AAU-512\SDR_testing\Amplitude.txt", "r")
+# # Construct the file name with date and time
+# date_time = datetime.now().strftime("%d %m %Y %H %M %S")
+# text_file = os.path.join(text_file_position, f"Steering_angles_{date_time}.txt")
 
+# # Print the full file path
+# print(text_file)
 
-# # steer_angle = np.loadtxt("Steering angles.txt", delimiter=", ")
-# # Amplitude = np.loadtxt("Amplitude.txt", delimiter=", ")
+# # Create the file
+# with open(os.path.normpath(text_file), "w") as f:
+#     # Add content to the file if needed
+#     f.write("Your content goes here")
 
-# # Plotting
-# plt.plot(steer_angle, Amplitude, marker='o')
-# plt.xlabel('Steer Angle')
-# plt.ylabel('Amplitude')
-# plt.title('Steer Angle vs. Amplitude')
-# plt.savefig('output_plot.png')
-print(date_time)
-path = ("C:\Users\caspe\OneDrive - Aalborg Universitet\GIT\P5-EIT-AAU-512\SDR_testing\data_folder\myfile.txt" + str(date_time))
-print(path)
-f = open(r"C:\Users\caspe\OneDrive - Aalborg Universitet\GIT\P5-EIT-AAU-512\SDR_testing\data_folder\myfile.txt", "x")
-f.write
+current_folder = os.getcwd()
+text_file_position = os.path.join(current_folder, "data_folder")
+
+# Create the data_folder if it doesn't exist
+if not os.path.exists(text_file_position):
+    os.makedirs(text_file_position)
+
+date_time = datetime.now().strftime("%d %m %Y %H %M %S")
+text_file = os.path.join(text_file_position, f"Steering_angles_{date_time}.txt")
+print(text_file)
+# Write content to the file
+with open(os.path.normpath(text_file), "w") as f:
+    f.write("Your content goes here")
+
