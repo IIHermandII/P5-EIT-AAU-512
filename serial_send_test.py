c47862132d00ce58467e0eb8c958824dab7e8495
HerpSlerp <asbjorn.herbsleb@gmail.com>
2023-12-06 13:39:31 +0100

    python send test brugt til test

--- serial_send_test.py
+++ serial_send_test.py
@@ -1,17 +1,15 @@
 import time 
 import serial
 
-SerialObj = serial.Serial('COM20', baudrate=115200, bytesize=8, parity='N', stopbits=1)
-SerialObj.close()
+SerialObj = serial.Serial('COM19', baudrate=115200, bytesize=8, parity='N', stopbits=1)
+time.sleep(2)
 
-for i in range(1,10):
-    SerialObj = serial.Serial('COM20', baudrate=115200, bytesize=8, parity='N', stopbits=1)
-    
-    # input()
-    time.sleep(2)
-    SerialObj.write(bytes(str(4), "utf-8"))  # Transmit input to Arduino
-    print("just sendt a 4")
-    time.sleep(2)  # Optional delay to ensure data is sent before proceeding
-    SerialObj.close()
+while True:
+    input_string=input("Please input an int between -90 to 90 (int)")
+    SerialObj.write(bytes(str(input_string), "utf-8"))  # Transmit input to Arduino
+    print("just sendt a: ", input_string)
+    if input_string == "B":
+        break
 
+print("breake!")
 # Close the serial port when done (this line will not be reached in the current script)
